1a9fa2a96e531a983dc4970fa88c81b7edea5728
markchern <13zxchen3@gmail.com>
2022-03-11 15:07:16 +0800

    去掉注释

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    // 你的所有代码都应该写在这里
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
         if (targetImage.classList.contains("small")) {
