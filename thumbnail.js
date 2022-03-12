7b4b07a64fda72d84b6303751758eb15cd871dcb
markchern <13zxchen3@gmail.com>
2022-03-12 09:21:58 +0800

    pic

--- thumbnail.js
+++ thumbnail.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-        if (targetImage.classList.contains("small")) {
-            targetImage.classList.remove("small");
-        } else {
-            targetImage.classList.add("small");
-        }
+        targetImage.classList.remove("small");
+        // if (targetImage.classList.contains("small")) {
+        //     targetImage.classList.remove("small");
+        // } else {
+        //     targetImage.classList.add("small");
+        // }
         
     });
 });
